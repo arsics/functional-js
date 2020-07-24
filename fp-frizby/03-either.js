4d644b20da6b800bfd99681aa99c16d580533165
Sasha Arsic <arsic.sasha@gmail.com>
2020-07-24 13:55:49 +0200

    fp-frizby: adding fold to Right and Left

--- fp-frizby/03-either.js
+++ fp-frizby/03-either.js
@@ -1,14 +1,16 @@
 const Right = x => ({
     map: f => Right(f(x)),
+    fold: (f, g) => g(x),
     inspect: () => `Right(${x})`
 });
 const Left = x => ({
     map: f => Left(x),
+    fold: (f, g) => f(x),
     inspect: () => `Left(${x})`
 });
 
-const resultRight = Right(3).map(n => n + 1).map(n => n / 2);
-console.log(resultRight.inspect());
+const resultRight = Right(3).map(n => n + 1).map(n => n / 2).fold(x => 'error', x => x);
+console.log(`resultRight: ${resultRight}`);
 
-const resultLeft = Left(3).map(n => n + 1).map(n => n / 2);
-console.log(resultLeft.inspect()); // Left does not apply f to x in map
+const resultLeft = Left(3).map(n => n + 1).map(n => n / 2).fold(x => 'error', x => x);
+console.log(`resultLeft: ${resultLeft}`);
