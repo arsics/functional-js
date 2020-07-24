aa3ca1efd1bab12d95fce4669db07885848de652
Sasha Arsic <arsic.sasha@gmail.com>
2020-07-24 14:03:29 +0200

    fp-frizby: function invocation with error result - Either not used

--- fp-frizby/03-either.js
+++ fp-frizby/03-either.js
@@ -14,3 +14,14 @@ console.log(`resultRight: ${resultRight}`);
 
 const resultLeft = Left(3).map(n => n + 1).map(n => n / 2).fold(x => 'error', x => x);
 console.log(`resultLeft: ${resultLeft}`);
+
+// ---
+
+const findColor = name =>
+    ({red: '#ff4444', blue: '#3b5998', yellow: '#fff68f'})[name];
+
+const resultRed = findColor('red').slice(1).toUpperCase();
+console.log(`Result for 'red': ${resultRed}`);
+
+const resultGreen = findColor('green').slice(1).toUpperCase(); // throws
+console.log(`Result for 'green': ${resultGreen}`);
