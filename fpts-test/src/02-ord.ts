024ecb117b888d6d94fe0df29ad6f389ef5a7df2
Sasha Arsic <arsic.sasha@gmail.com>
2020-09-10 11:27:01 +0200

    fp-ts: Getting started with fp-ts: Ord (2)
    Advanced Ord use with fp-ts implementation import

--- fpts-test/src/02-ord.ts
+++ fpts-test/src/02-ord.ts
@@ -1,17 +1,31 @@
 // https://dev.to/gcanti/getting-started-with-fp-ts-ord-5f1e
 
-import { Eq } from 'fp-ts/Eq';
+import { Ord, fromCompare, contramap } from 'fp-ts/lib/Ord'
 
-// type class Ord, intended to contain types that admit a total ordering:
+// instance of Ord for the type number by using the helper fn fromCompare:
+const ordNumber: Ord<number> = fromCompare((x, y) => x < y ? -1 : x > y ? 1 : 0);
 
-type Ordering = -1 | 0 | 1
-
-interface Ord<A> extends Eq<A> {
-  readonly compare: (x: A, y: A) => Ordering
+// we can easily create a min fn when given an Ord instance for type A:
+function min<A>(O: Ord<A>): (x: A, y: A) => A {
+    return (x, y) => O.compare(x, y) === -1 ? x : y;
 }
 
-// instance of Ord for the type number:
-const ordNumber: Ord<number> = {
-    compare: (x, y) => x < y ? -1 : x > y ? 1 : 0,
-    equals: (x, y) => x === y
+console.log('minimum of 5 & 3 is: ' + min(ordNumber)(5, 3))
+
+// again, with contramap we can derive an Ort instance for Person:
+type Person = {
+  name: string
+  age: number
 }
+
+const ordPerson: Ord<Person> = contramap((p: Person) => p.age)(ordNumber)
+
+const topalovici = [
+  { name: 'Laki', age: 44 },
+  { name: 'Aksentije', age: 102 },
+  { name: 'Milutin', age: 79 },
+  { name: 'Pantelija', age: 150 },
+  { name: 'Mirko', age: 24 },
+  { name: 'Maksimilijan', age: 126 }
+];
+console.log('Ordered Topalovici: ' + topalovici.sort(ordPerson.compare).map(t => t.name))
