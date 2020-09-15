8c9377a90048262637d5b541fbc767d41bc0c9d0
Sasha Arsic <arsic.sasha@gmail.com>
2020-09-15 15:20:25 +0200

    fp-ts: Getting started with fp-ts: Semigroup (2)
    Advanced examples

--- fpts-test/src/03-semigroup.ts
+++ fpts-test/src/03-semigroup.ts
@@ -1,44 +1,49 @@
 // https://dev.to/gcanti/getting-started-with-fp-ts-semigroup-2mf7
 
-interface Semigroup<A> {
-  concat: (x: A, y: A) => A
+import { ordNumber } from 'fp-ts/lib/Ord'
+import { Semigroup, getMeetSemigroup, getJoinSemigroup, getStructSemigroup, semigroupSum, semigroupAll, getFunctionSemigroup, fold, semigroupProduct } from 'fp-ts/lib/Semigroup'
+
+// Derive a semigroup instance from an Ord instance:
+
+/** Takes the minimum of two values */
+const semigroupMin: Semigroup<number> = getMeetSemigroup(ordNumber)
+console.log('semigroupMin(2,1):' + semigroupMin.concat(2, 1)) // 1
+
+/** Takes the maximum of two values  */
+const semigroupMax: Semigroup<number> = getJoinSemigroup(ordNumber)
+console.log('semigroupMax(2,1):' + semigroupMax.concat(2, 1)) // 2
+
+// Derive a Semigroup instance for a complex type:
+type Point = {
+  x: number
+  y: number
 }
+const semigroupPoint: Semigroup<Point> = getStructSemigroup({
+  x: semigroupSum,
+  y: semigroupSum
+})
+console.log('semigroupPoint({x:1,y:2}, {x:5,y:6}):' + JSON.stringify(semigroupPoint.concat({x:1,y:2}, {x:5,y:6})));
 
-/** number `Semigroup` under multiplication */
-const semigroupProduct: Semigroup<number> = {
-  concat: (x, y) => x * y
-}
+// combinator that allows to derive a Semigroup instance for functions:
+// `semigroupAll` is the boolean semigroup under conjunction
+const semigroupPredicate: Semigroup<(p: Point) => boolean> = getFunctionSemigroup(
+  semigroupAll
+)<Point>()
 
-/** number `Semigroup` under addition */
-const semigroupSum: Semigroup<number> = {
-  concat: (x, y) => x + y
-}
+// Now we can "merge" two predicates on Points:
+const isPositiveX = (p: Point): boolean => p.x >= 0
+const isPositiveY = (p: Point): boolean => p.y >= 0
 
-const semigroupString: Semigroup<string> = {
-  concat: (x, y) => x + y
-}
+const isPositiveXY = semigroupPredicate.concat(isPositiveX, isPositiveY)
 
-// What if, given a type A, you can't find an associative operation on A?
-// You can create a (trivial) semigroup instance for every type just using the following constructions:
+console.log(isPositiveXY({ x: 1, y: 1 })) // true
+console.log(isPositiveXY({ x: 1, y: -1 })) // false
+console.log(isPositiveXY({ x: -1, y: 1 })) // false
+console.log(isPositiveXY({ x: -1, y: -1 })) // false
 
-/** Always return the first argument */
-function getFirstSemigroup<A = never>(): Semigroup<A> {
-  return { concat: (x, y) => x }
-}
+// folding:
+const sum = fold(semigroupSum)
+console.log(sum(0, [1, 2, 3, 4])) // 10
 
-/** Always return the second argument */
-function getLastSemigroup<A = never>(): Semigroup<A> {
-  return { concat: (x, y) => y }
-}
-
-// Another technique is to define a semigroup instance for Array<A> (*), called the free semigroup of A
-function getArraySemigroup<A = never>(): Semigroup<Array<A>> {
-  return { concat: (x, y) => x.concat(y) }
-}
-
-// and map the elements of A to the singleton elements of Array<A>:
-function of<A>(a: A): Array<A> {
-  return [a]
-}
-
-// (The free semigroup of A is the semigroup whose elements are all possible non-empty finite sequences of elements of A)
+const product = fold(semigroupProduct)
+console.log(product(1, [1, 2, 3, 4])) // 24
